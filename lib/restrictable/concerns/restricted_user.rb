0392c8729892ba32d6684f5f76c5080dd730b268
Brookie Guzder-Williams <brook.williams@gmail.com>
2015-02-24 08:01:00 -0800

    dynamic class/instance methods working

--- lib/restrictable/concerns/restricted_user.rb
+++ lib/restrictable/concerns/restricted_user.rb
@@ -7,16 +7,6 @@ module Restrictable
     #
 
     module ClassMethods
-
-      def roles
-        [ 
-          'super',
-          'admins',
-          'client',
-          'translator'
-        ]
-      end
-      
       def supers
         where(role: 'super')
       end
@@ -27,6 +17,40 @@ module Restrictable
 
       def admins_for_group group
         admins.where(group: group)
+      end
+    end
+
+    #
+    # included
+    #
+
+    included do
+      validates_presence_of :role
+      unless restrictable_models.blank?
+        restrictable_models.each do |restrictable_model|
+          belongs_to restrictable_model["class"].underscore.to_sym, class_name: restrictable_model_to_class_name(restrictable_model)
+        end
+      end
+    end
+
+    #
+    # instance methoods
+    #
+
+    def is_super?
+        @is_super ||= (role == 'super')
+    end
+
+    def is_admin?
+      @is_admin ||= (role == 'admin')
+    end
+
+  # private
+
+    module ClassMethods
+
+      def roles
+        [ 'super', 'admins' ] | custom_roles
       end
 
       #
@@ -52,8 +76,8 @@ module Restrictable
         parts = method_name.split("_for_")
         if parts.length == 2
           if custom_roles.include?(parts[0].singularize)
-            ru_rm_arr = restricted_user_restrictable_models.select{|ru_rm| ru_rm["class"].underscore == parts[1] }
-            if ru_rm_arr.length > 0
+            restrictable_model = restrictable_model_find_by_class_name parts[1]
+            if restrictable_model
               {
                 roles: parts[0],
                 object: parts[1]
@@ -65,11 +89,9 @@ module Restrictable
 
       def method_missing(method_call,*arguments)
         hash = method_hash(method_call)
-        puts "mm2 -- #{hash}"
         if hash.nil?
           super
         else
-        puts "send -- #{hash[:method]},#{hash[:param]},#{arguments}"
           send(hash[:method],hash[:param],arguments)
         end
       end
@@ -101,142 +123,84 @@ module Restrictable
         @custom_roles ||= Restrictable.config["custom_roles"]
       end
 
-      def restricted_user_restrictable_models
-        @restricted_user_restrictable_models ||= Restrictable.config["restricted_user_restrictable_models"]
+      def restrictable_models
+        @restrictable_models ||= Restrictable.config["restrictable_models"]
       end
 
-      def ru_rm_to_class_name ru_rm
-        module_part = "#{ru_rm['engine']}::" unless ru_rm['engine'].blank?
-        "#{module_part}#{ru_rm["class"]}"
+      def restrictable_model_to_class_name restrictable_model
+        module_part = "#{restrictable_model['engine']}::" unless restrictable_model['engine'].blank?
+        "#{module_part}#{restrictable_model["class"]}"
+      end
+
+      def restrictable_model_find_by_class_name class_name
+        restrictable_models.select{|restrictable_model| restrictable_model["class"].underscore == class_name }.first
       end
 
     end
 
-    #
-    # included
-    #
-
-    included do
-      validates_presence_of :role
-      unless restricted_user_restrictable_models.blank?
-        restricted_user_restrictable_models.each do |ru_rm|
-          belongs_to ru_rm["class"].to_sym, class_name: ru_rm_to_class_name(ru_rm)
-        end
-      end
-    end
-
-    #
-    # instance methoods
-    #
-
-    def groups 
-      unless group.nil?
-        if is_super?
-          Group.all
-        else
-          group.descendents(true)
-        end
-      end
-    end
-
-    def is_super?
-        @is_super ||= (role == 'super')
-    end
-
-    def is_admin?
-      @is_admin ||= (role == 'admin')
-    end
-
-  # private
 
     #
     # Dynamic Methods
     #
 
-    # def role_check dynamic_role, args
-    #   role == dynamic_role
-    # end
-
-    # def role_check_role method_name
-    #   if method_name.starts_with?("is_") && method_name.ends_with?("?")
-    #     role_check_class_name = method_name.gsub(/^is_/,'').gsub(/\?$/,"")
-    #     if custom_roles.include?(role_check_class_name.pluralize)
-    #       role_check_class_name
-    #     end
-    #   end
-    # end
-
-    # def objects dynamic_role, args
-    #   where(role: dynamic_role)
-    # end
-
-    # def objects_role method_name
-    #   if custom_roles.include?(method_name)
-    #     method_name
-    #   end
-    # end
-
-    # def roles_for dynamic_hash, object
-    #   send(dynamic_hash[:roles]).where(dynamic_hash[:object].to_sym => object)
-    # end
-
-    # def roles_for_hash method_name
-    #   parts = method_name.split("_for_")
-    #   if parts.length == 2
-    #     if custom_roles.include?(parts[0])
-    #       ru_rm_arr = restricted_user_restrictable_models.select{|ru_rm| ru_rm["class"].underscore == parts[1] }
-    #       if ru_rm.length > 0
-    #         {
-    #           roles: parts[0],
-    #           object: parts[1]
-    #         }
-    #       end
-    #     end
-    #   end
-    # end
-
-    def method_missing(method_call,*arguments)
-      puts "mm i #{arguments}"
-      puts "mm i #{arguments}"
-      puts "mm i #{arguments}"
-      puts "mm i #{arguments}"
-      puts "mm i #{arguments}"
-
-      send("id")
-      # hash = method_hash(method_call)
-      # if hash.nil?
-      #   super
-      # else
-      #   send(hash[:method],hash[:param],*arguments)
-      # end
+    def role_check dynamic_role
+      role == dynamic_role
     end
 
-    # def method_hash(method_call)
-    #   method_name = method_call.to_s
-    #   method_role_name = role_check_role(method_name)
-    #   if method_role_name
-    #     { 
-    #       method: 'role_check',
-    #       param: method_role_name
-    #     }
-    #   else
-    #     method_role_name = objects_role(method_name)
-    #     if method_role_name
-    #       { 
-    #         method: 'objects',
-    #         param: method_role_name
-    #       }
-    #     else
-    #       roles_for_dict = roles_for_hash(method_name)
-    #       if method_role_name
-    #         { 
-    #           method: 'roles_for',
-    #           param: roles_for_dict
-    #         }
-    #       end
-    #     end
-    #   end    
-    # end
+    def role_check_role method_name
+      if method_name.starts_with?("is_") && method_name.ends_with?("?")
+        role_check_class_name = method_name.gsub(/^is_/,'').gsub(/\?$/,"")
+        if custom_roles.include?(role_check_class_name)
+          role_check_class_name
+        end
+      end
+    end
+
+    def objects dynamic_hash
+      if is_super?
+        self.class.restrictable_model_to_class_name(dynamic_hash).constantize.all
+      else
+        dynamic_role = dynamic_hash["class"].underscore
+        top_obj = send(dynamic_role)
+        unless top_obj.nil?
+          top_obj.descendents(true)
+        end
+      end
+    end
+
+    def objects_hash method_name
+      if method_name.pluralize == method_name
+        self.class.restrictable_model_find_by_class_name method_name.singularize
+      end
+    end
+
+    def method_missing(method_call,*arguments)
+      hash = method_hash(method_call)
+      if hash.nil?
+        super
+      else
+        send(hash[:method],hash[:param],*arguments)
+      end
+    end
+
+    def method_hash(method_call)
+      method_name = method_call.to_s
+      method_role_name = role_check_role(method_name)
+      if method_role_name
+        { 
+          method: 'role_check',
+          param: method_role_name
+        }
+      else
+        method_objects_dict = objects_hash(method_name)
+        if method_objects_dict
+          { 
+            method: 'objects',
+            param: method_objects_dict
+          }
+        end
+      end    
+    end
 
     def respond_to_missing?(method_call, include_private = false)
       !method_hash(method_call).nil? || super
@@ -246,8 +210,9 @@ module Restrictable
       self.class.custom_roles
     end
 
-    def restricted_user_restrictable_models
-      self.class.restricted_user_restrictable_models
+    def restrictable_models
+      self.class.restrictable_models
     end
+
   end
 end
