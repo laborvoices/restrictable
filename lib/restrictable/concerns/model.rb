74b7a3dbde1d2f623fa21ef026de17ef65841c74
Brookie Guzder-Williams <brook.williams@gmail.com>
2015-06-08 11:35:21 -0700

    all_from

--- lib/restrictable/concerns/model.rb
+++ lib/restrictable/concerns/model.rb
@@ -16,24 +16,56 @@ module Restrictable
         restricted_user_model_class_name.underscore.gsub("/","_").pluralize
       end
 
+
+      def all_from admin, through_class, object_class
+        through_instance_name = through_class.name.demodulize.underscore
+        through_id_name = "#{through_instance_name}_id".to_sym
+        
+        #
+        # Rails Bug || Oddity:  [Admin belongs_to :group]
+        #
+        # Admin.method_defined? "group_id" => false
+        # Admin.new.respond_to? "group_id" => true
+        # Admin.method_defined? "group_id" => true
+        #
+        if object_class.new.respond_to? through_id_name
+          from = object_class
+        else
+          through_index_name = through_instance_name.pluralize
+          if object_class.new.respond_to? through_index_name
+            from = object_class.joins(through_index_name.to_sym)
+          end
+        end
+
+        if from 
+          instance_ids = admin.send(through_instance_name).descendents(true).select(:id)
+          from.where("#{through_id_name} in (#{instance_ids.pluck(:id).join(',')}) ")
+        else
+          raise ArgumentError, 'Restrictable Configuration Error[all_from]'
+        end
+      end
+
       #
       # Main Class Methods
       #
 
-      def restricted_through through_method
+      def restricted_through through_method, object_class = nil
         @through_method = through_method
+        @object_class = object_class
       end
 
-      def for_admin current_admin 
-        if current_admin.is_super?
+      def for_admin admin 
+        if admin.is_super?
           all
         elsif @through_method.nil?
           none
         elsif @through_method == :restricted_user
           joins(restricted_user_table_name.to_sym)
-              .where("#{restricted_user_table_name}.id = ?",current_admin.restrictable_role_id)
+            .where("#{restricted_user_table_name}.id = ?",admin.restrictable_role_id)
+        elsif @object_class
+          all_from admin, @through_method, @object_class
         else
-          send(@through_method,current_admin.face)
+          send(@through_method,admin.face)
         end
       end
     end
