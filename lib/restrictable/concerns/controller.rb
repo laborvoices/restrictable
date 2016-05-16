5943eaed1ba354d0fbf74ecee2118d4a69377e25
Maggie Epps <maggieepps@laborvoices.com>
2016-05-16 11:47:56 -0400

    Sped up object query

--- lib/restrictable/concerns/controller.rb
+++ lib/restrictable/concerns/controller.rb
@@ -147,7 +147,7 @@ module Restrictable
       object = self.class.restricted_object
       object = instance_variable_get("@#{controller_name.singularize}") if object.nil?
       unless object.blank?
-        object.class.for_admin(current_admin).include?(object)
+        object.class.for_admin(current_admin).pluck(:id).include?(object.id)
       end
     end
 
