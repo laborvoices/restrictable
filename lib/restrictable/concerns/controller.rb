65d4592502cf3c05614a267245b6a32938c3fc6a
Maggie Epps <maggieepps@laborvoices.com>
2016-05-16 11:55:34 -0400

    Updated object assignment

--- lib/restrictable/concerns/controller.rb
+++ lib/restrictable/concerns/controller.rb
@@ -144,8 +144,7 @@ module Restrictable
     end
 
     def is_valid_admin?
-      object = self.class.restricted_object
-      object = instance_variable_get("@#{controller_name.singularize}") if object.nil?
+      object = self.class.restricted_object || instance_variable_get("@#{controller_name.singularize}")
       unless object.blank?
         object.class.for_admin(current_admin).pluck(:id).include?(object.id)
       end
